eb96e5f421998a2701fecb4c1eb60afc86148c88
Ellizzabeth <99212744+Ellizzabeth@users.noreply.github.com>
2022-04-05 16:05:33 +0300

    list

--- DZ6_list/index.js
+++ DZ6_list/index.js
@@ -12,6 +12,43 @@ const tasks = [
 /*var x = document.getElementById("list");
 x.querySelector(".listclass").innerHTML = "Hello World!";*/
 
+function checkboxClick(event){
+    let checkboxes = document.querySelectorAll('input[type="checkbox"]');
+            checkboxes.forEach((checkbox) => {
+            var label = document.getElementById("label_" + checkbox.value);
+            if (checkbox.checked) label.style.setProperty("text-decoration", "line-through");
+            else label.style.setProperty("text-decoration", "none");
+                 });
+            
+}
+
+
+var CheckBoxID = 0;
+function addTask(taskName){
+    var div = document.getElementById("list");
+    var checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.id = 'cb' + CheckBoxID;
+    checkbox.name = 'cb' + CheckBoxID;
+    checkbox.value = 'cb' + CheckBoxID;
+
+    checkbox.addEventListener('click', checkboxClick);
+      div.appendChild(checkbox);
+
+
+      var label = document.createElement('label');
+      label.id = 'label_cb' + CheckBoxID;
+      label.for = 'cb' + CheckBoxID;
+      label.innerHTML = taskName;
+      div.appendChild(label);
+
+      var br = document.createElement("br");
+      div.appendChild(br);
+
+      CheckBoxID++;
+
+}
+
 function loadPage()
 {
   //var elem = document.getElementById(1);
@@ -20,43 +57,28 @@ function loadPage()
   var listElement = div.querySelector(".listclass");
   //if(listElement.hasAttribute("vvalue")) 
   alert(listElement.vvalue);*/
-  var div = document.getElementById("list");
-  var i=0;
-  for(const task of tasks){
-   /* var child = document.createTextNode("label");
-    div.appendChild(child);
-    //child.value = "dddd";
-    firstEl = document.getElementById("yyy");
-    //div.insertBefore(child, firstEl);*/
-
-      var checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    checkbox.id = 'cb'+i;
-    checkbox.name = 'cb';
-    checkbox.value = 'cb'+i;
-      div.appendChild(checkbox);
-
-
-      var label = document.createElement('label');
-      label.for = 'cb'+i;
-      label.innerHTML = task.text;
-      div.appendChild(label);
-
-      var br = document.createElement("br");
-      div.appendChild(br);
-
-
   
-    // div.insertBefore(checkbox, child);
+ 
+  for(const task of tasks){
+        
+      addTask(task.text);
+  
   } 
- /* var newDiv = document.createElement("div"); 
-  // and give it some content 
-  var newContent = document.createTextNode("Hi there and greetings!"); 
-  // add the text node to the newly created div
-  newDiv.appendChild(newContent);  
 
-  // add the newly created element and its content into the DOM 
-  var currentDiv = document.getElementById("list"); 
-  document.body.insertBefore(newDiv, currentDiv); */
+}
+function onclickCreateTask(){
+    var newTask = document.getElementById("newTask");
+    addTask(newTask.value);
+        
+     
 }
 
+
+
+/*const div1 = document.getElementById('div1');
+//=> <div id="div1">Hi Champ!</div>
+
+const exampleAttr= div1.getAttribute('id');
+//=> "div1"
+
+const align = div1.getAttribute('align')*/  
